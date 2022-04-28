33a4cc265714249ce49275b0f4caa72605746bb8
guk, mu <garuru7080@gmail.com>
2022-04-28 17:16:19 +0900

    #000 voice update

--- js/chapter1.js
+++ js/chapter1.js
@@ -17,6 +17,21 @@ monogatari.characters ({
 	},
 });
 
+monogatari.assets ('voices', {
+	'000_han_000' : 'testScene/han/000_han_000.mp3',
+	'000_han_001' : 'testScene/han/000_han_001.mp3',
+	'000_han_002' : 'testScene/han/000_han_002.mp3',
+	'000_han_003' : 'testScene/han/000_han_003.mp3',
+	'000_han_004' : 'testScene/han/000_han_004.mp3',
+	'000_han_005' : 'testScene/han/000_han_005.mp3',
+	'000_han_006' : 'testScene/han/000_han_006.mp3',
+	'000_han_007' : 'testScene/han/000_han_007.mp3',
+	'000_han_008' : 'testScene/han/000_han_008.mp3',
+	'000_han_009' : 'testScene/han/000_han_009.mp3',
+	'000_han_010' : 'testScene/han/000_han_010.mp3',
+});
+
+
 monogatari.script ({
 	// 챕터 선택지
 	'choose_chapter1_or_testscene' :[
@@ -56,6 +71,7 @@ monogatari.script ({
 		'kang (긁적이며) 아.. 네.',
 		'w 안심, 채끝살로 준비해드리겠습니다. 와인은 어떻게 해드릴까요?',
 		'kang (여름의 눈치를 보며) 그...그것도 추천으로.',
+		'play voice 000_han_000',
 		'me (입을 막으며 푸흐흐 웃는 소리)',
 		'w 독일 와인, 러시아 와인 종류로 추천드립니다.',
 		'kang 단 걸 좋아하니, 단걸로 부탁합니다.',
@@ -71,13 +87,16 @@ monogatari.script ({
 		'show character kang normal_R fadeIn with duration 2s at center',
 		'익숙하지 않은 듯한 성찬의 바보같은 모습을 본 여름은 웃음을 참다가 결국 크게 웃어버린다. 성찬은 부끄러운 듯 얼굴을 붉히며 당황한다.',
 
+		'play voice 000_han_001',
 		'me (웃으며) 큭..큭하하하! 하하하! 웃겨라!',
 		'kang  왜 자꾸 웃습니까.',
+		'play voice 000_han_002',
 		'me (크게 웃는 소리 / 재미있다는 듯 눈물을 닦으며 놀리듯) 그냥, 우리 대표님 추천 좋아하시나보다~',
 
 		'(효과음) 웨이터가 구두를 신고 다가오는 소리 (타일 밟는 소리) + (효과음) 카트를 끌고 오는 소리',
 		'play sound dol_tap',
 
+		'play voice 000_han_003',
 		'me (큭큭 거리며 웃는다)',
 
 		'(효과음) 접시들을 테이블에 올려놓는 소리 (빵을 담아 놓는 나무재질 바구니 / 잼그릇과 잼칼 / 스프 2접시)',
@@ -85,21 +104,28 @@ monogatari.script ({
 
 		'kang 한여름씨. 그렇게 웃기십니까?',
 		'play sound breadknife',
+		'play voice 000_han_004',
 		'me ((효과음) 잼칼 가볍게 드는 소리 / 잼칼을 들어올려 빵에 잼을 바른다) 하하하. 대표님도 귀여운 구석이 있네요? 직원들이 이런 모습을 보면 깬다고 할걸요? (빵을 살짝 입에 물고) 완전 상남자스타일인 줄 알던데.',
 		'kang ...여름씨한텐 어떤데요? 저같은 타입, 별롭니까?',
+		'play voice 000_han_005',
 		'me (사레들린 기침) / ((효과음) 급하게 물 마시는 소리 / 물마시고 잘못들었다는 듯이) ...뭐라구요?',
 		'kang ….여름씨는 귀여운 남자 어떤데요?',
+		'play voice 000_han_006',
 		'me ...무슨 뜻이에요?',
 		'kang (고민하다가 결심한 듯) 전, 여름씨가 좋아하는 타입이 저였으면 ..',
 		'play sound dish_drop_s',
+		'play voice 000_han_007',
 		'me ((효과음) 식기를 탁 놓는 소리 / 식기를 내려놓으며 O.L) 어! 대표님, 그거 완전 고백멘트였어요! 저라서 다행이지, 누가 들으면 오해한다고요! 소문이라도 나면 어쩌려고 그러세요?',
 
 		'kang ...오해하라고 하는 말입니다. (반응이 없자) 장난치는 거 아닙니다. 내가 무슨 말을 하고 있는 지 충분히 알고 있어요. 소문나면 여름씨가 낙하산이라고 손가락질 받을거라는 것도 잘 알고, 여름씨가 저에게 관심없다는 것도.. (고민하다가) ...알아요.',
+		'play voice 000_han_008',
 		'me (조용히 낮게 발끈하며) 그걸 아는 사람이..!',
 		'kang (O.L) 이기적이라고 생각할 수도 있는데, 그냥 오늘은 제 마음만 전하고 싶었습니다. 내가 이런 마음이니까… 그냥..',
+		'play voice 000_han_009',
 		'me (크게 들숨, 날숨) 알겠어요.',
 		'kang (잘못들었다는 듯) 네?',
 		'play sound breadknife',
+		'play voice 000_han_010',
 		'me ((효과음) 숟가락 들어올리는 소리 / 숟가락을 들며) 알았으니까 밥이나 먹어요. 오늘 아무것도 못 먹었을 거 아녜요?',
 		'kang (밝게 웃는 호) 네.',
 
